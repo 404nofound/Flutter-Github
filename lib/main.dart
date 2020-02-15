2d7807301fa34104d1da33bd1b3aaa93f8a7b424
Eddy <eddynjupt@qq.com>
2020-02-15 00:35:03 -0500

    Animation II

--- lib/main.dart
+++ lib/main.dart
@@ -71,6 +71,7 @@ class MyApp extends StatelessWidget {
         "scale_animation":(context) => ScaleAnimationRoute(),
         "scale_animation_better":(context) => ScaleAnimationRoute1(),
         "stagger_animation":(context) => StaggerRoute(),
+        "animated_switcher":(context) => AnimatedSwitcherCounterRoute(),
       },
 
     );
@@ -501,6 +502,7 @@ class _SecondPage extends State<SecondPage> {
                 Navigator.of(context).pushNamed("scroll_notification");
               },
             ),
+
           ],
         ),
       ),
@@ -642,6 +644,13 @@ class _ThirdPage extends State<ThirdPage> {
               textColor: Colors.blue,
               onPressed: () {
                 Navigator.of(context).pushNamed("stagger_animation");
+              },
+            ),
+            FlatButton(
+              child: Text("动画切换"),
+              textColor: Colors.blue,
+              onPressed: () {
+                Navigator.of(context).pushNamed("animated_switcher");
               },
             ),
           ],
@@ -3736,3 +3745,122 @@ class _StaggerRouteState extends State<StaggerRoute> with TickerProviderStateMix
     );
   }
 }
+
+class AnimatedSwitcherCounterRoute extends StatefulWidget {
+  const AnimatedSwitcherCounterRoute({Key key}) : super(key: key);
+
+  @override
+  _AnimatedSwitcherCounterRouteState createState() => _AnimatedSwitcherCounterRouteState();
+}
+
+class _AnimatedSwitcherCounterRouteState extends State<AnimatedSwitcherCounterRoute> {
+  int _count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("动画切换"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            AnimatedSwitcher(
+              duration: Duration(milliseconds: 500),
+              transitionBuilder: (Widget child, Animation<double> animation) {
+                //var tween = Tween<Offset>(begin: Offset(1, 0), end: Offset(0, 0));
+                return SlideTransitionX(
+                  child: child,
+                  direction: AxisDirection.down, //上入下出
+                  position: animation,
+                );
+              },
+              child: Text(
+                "$_count",
+                key: ValueKey<int>(_count),
+                style: Theme.of(context).textTheme.display1,
+              ),
+            ),
+            RaisedButton(
+              child: Text("+1"),
+              onPressed: () {
+                setState(() {
+                  _count += 1;
+                });
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SlideTransitionX extends AnimatedWidget {
+  SlideTransitionX({
+    Key key,
+    @required Animation<double> position,
+    this.transformHitTests = true,
+    this.direction = AxisDirection.down,
+    this.child,
+  })
+      : assert(position != null),
+        super(key: key, listenable: position) {
+    // 偏移在内部处理
+    switch (direction) {
+      case AxisDirection.up:
+        _tween = Tween(begin: Offset(0, 1), end: Offset(0, 0));
+        break;
+      case AxisDirection.right:
+        _tween = Tween(begin: Offset(-1, 0), end: Offset(0, 0));
+        break;
+      case AxisDirection.down:
+        _tween = Tween(begin: Offset(0, -1), end: Offset(0, 0));
+        break;
+      case AxisDirection.left:
+        _tween = Tween(begin: Offset(1, 0), end: Offset(0, 0));
+        break;
+    }
+  }
+
+
+  Animation<double> get position => listenable;
+
+  final bool transformHitTests;
+
+  final Widget child;
+
+  //退场（出）方向
+  final AxisDirection direction;
+
+  Tween<Offset> _tween;
+
+  @override
+  Widget build(BuildContext context) {
+    Offset offset = _tween.evaluate(position);
+    if (position.status == AnimationStatus.reverse) {
+      switch (direction) {
+        case AxisDirection.up:
+          offset = Offset(offset.dx, -offset.dy);
+          break;
+        case AxisDirection.right:
+          offset = Offset(-offset.dx, offset.dy);
+          break;
+        case AxisDirection.down:
+          offset = Offset(offset.dx, -offset.dy);
+          break;
+        case AxisDirection.left:
+          offset = Offset(-offset.dx, offset.dy);
+          break;
+      }
+    }
+    return FractionalTranslation(
+      translation: offset,
+      transformHitTests: transformHitTests,
+      child: child,
+    );
+  }
+}
+
